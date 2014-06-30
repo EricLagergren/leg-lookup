7dd1c2f35a7aea7b53aed4bc6b29c9652d94415c
Eric <ericscottlagergren@gmail.com>
2014-06-29 18:52:51 -0700

    quick update

---
+++ database/hash.py
@@ -0,0 +1,32 @@
+import urllib
+import hashlib
+import shutil
+from subprocess import call
+
+senatechecksum = hashlib.md5(open('senate.json').read()).hexdigest()
+housechecksum = hashlib.md5(open('house.json').read()).hexdigest()
+
+senate = urllib.urlretrieve('http://api.nytimes.com/svc/politics/v3/us/legislative/congress/113/senate/members.json?api-key=d03b1f0e94348b988961bde5c63fd7f9:15:69533710', 'newsenate.json')
+
+house = urllib.urlretrieve('http://api.nytimes.com/svc/politics/v3/us/legislative/congress/113/house/members.json?api-key=d03b1f0e94348b988961bde5c63fd7f9:15:69533710', 'newhouse.json')
+
+with open('newsenate.json', 'a') as file:
+	file.write(',')
+
+newsenatechecksum = hashlib.md5(open('newsenate.json').read()).hexdigest()
+newhousechecksum = hashlib.md5(open('newhouse.json').read()).hexdigest()
+
+if senatechecksum != newsenatechecksum:
+	shutil.copyfile('newsenate.json', 'senate.json')
+	print "copied senate"
+
+if housechecksum != newhousechecksum:
+	shutil.copyfile('newhouse.json', 'house.json')
+	print "copied house"
+
+call('cat senate.json house.json > congress.json', shell=True)
+
+
+
+
+
